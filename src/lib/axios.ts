be23b0d54f43b179f7e92d03abf83612ba0af971
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2025-12-01 23:03:49 +0900

    Feat: API URL 설정 및 토큰 재발급 인터셉터 세팅

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+import { setupInterceptors } from './interceptor';
+
 const API_TIMEOUT_MS = 5000;
 
-/** @todo API URL 추가 */
 export const instance = axios.create({
-  baseURL: '',
+  baseURL: process.env.LOCAL_API_BASE_URL,
   timeout: API_TIMEOUT_MS,
   headers: {
     'Content-Type': 'application/json',
@@ -12,10 +13,4 @@ export const instance = axios.create({
   withCredentials: true, // 액세스 토큰 및 리프레쉬 토큰 자동으로 첨부
 });
 
-/** @todo 401 에러 발생 시 리프레쉬 토큰으로 액세스 토큰 재발급 로직 추가 */
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    return Promise.reject(error);
-  },
-);
+setupInterceptors(instance);
